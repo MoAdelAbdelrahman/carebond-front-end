5e28d7a3341c779b602cd1aa2e9908b20ca76875
steven heung <stevenheung.ca@gmail.com>
2024-03-06 16:37:24 -0700

    Format Register

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -13,45 +13,40 @@
         <div class="container pt-lg-md">
             <div class="row justify-content-center">
                 <div class="col-lg-5">
-                    <card type="secondary" shadow
-                          header-classes="bg-white pb-5"
-                          body-classes="px-lg-5 py-lg-5"
-                          class="border-0">
+                    <card type="secondary" shadow header-classes="bg-white pb-5" body-classes="px-lg-5 py-lg-5"
+                        class="border-0">
                         <template>
                             <div class="text-muted text-center mb-3">
                                 <large>Register as</large>
                             </div>
                             <br>
                             <div class="btn-wrapper text-center">
-                                
                                 <button type="button" class="btn btn-outline-default">
-                                    <img slot="icon" src="img/icons/common/senior.png" style="width: 50px; height: auto;">
+                                    <img slot="icon" src="img/icons/common/senior.png"
+                                        style="width: 50px; height: auto;">
                                     SENIOR</button>
-                                        
-                                    
                             </div>
                             <br>
-                            
                             <div class="btn-wrapper text-center">
-                                
+
                                 <button type="button" class="btn btn-outline-default">
-                                    <img slot="icon" src="img/icons/common/carepro.png"  style="width: 50px; height: auto;">
+                                    <img slot="icon" src="img/icons/common/carepro.png"
+                                        style="width: 50px; height: auto;">
                                     CARE PROVIDER</button>
-                                        
-                                    
                             </div>
                         </template>
-                        
+
                     </card>
                 </div>
             </div>
         </div>
     </section>
 </template>
+
 <script>
 export default {
     name: "Register"
 };
 </script>
-<style>
-</style>
+
+<style></style>
