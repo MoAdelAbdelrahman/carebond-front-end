d292eb43eac9c14ae6d3fb680a4581704d5ca899
MoAdelAbdelrahman <mahmd.3adel@gmail.com>
2024-03-07 00:09:23 -0700

    UI change

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -15,26 +15,11 @@
                 <div class="col-lg-5">
                     <card type="secondary" shadow header-classes="bg-white pb-5" body-classes="px-lg-5 py-lg-5"
                         class="border-0">
-                        <template>
-                            <div class="text-muted text-center mb-3">
-                                <small>Sign in with</small>
-                            </div>
-                            <div class="btn-wrapper text-center">
-                                <base-button type="neutral">
-                                    <img slot="icon" src="img/icons/common/github.svg">
-                                    Github
-                                </base-button>
-
-                                <base-button type="neutral">
-                                    <img slot="icon" src="img/icons/common/google.svg">
-                                    Google
-                                </base-button>
-                            </div>
-                        </template>
+                        
 
                         <template>
                             <div class="text-center text-muted mb-4">
-                                <small>Or sign in with credentials</small>
+                                <small>Sign in with credentials</small>
                             </div>
                             <form role="form">
                                 <base-input alternative class="mb-3" placeholder="Email"
