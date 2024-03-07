8bcfa1cb5d8baa993d50edac2b17034ced78fad3
steven heung <stevenheung.ca@gmail.com>
2024-03-06 22:47:10 -0700

    Landing page

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -27,33 +27,59 @@
                                     go.</p>
                             </div>
                             <div class="col-lg-6">
-                                <img src="img/theme/img-1-1200x1000.jpg" alt="Image" class="img-fluid">
+                                <img src="../assets/section1.jpg" alt="Image" class="img-fluid">
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
             <!-- 1st Hero Variation -->
-            <section>
+            <section class="section-shaped my-1">
                 <div class="container">
-                    <div class="row row-grid align-items-center">
-                        <div class="col-md-6">
-                            <div class="card bg-default shadow border-0">
-                                <img src="img/theme/img-1-1200x1000.jpg" class="card-img-top">
+                    <div class="container shape-container d-flex">
+                        <div class="col px-0">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <img src="../assets/section2.png" alt="Image" class="img-fluid">
+                                </div>
+                                <div class="col-lg-6">
+                                    <h1 class="display-3">A beautiful Design System
+                                        <span>completed with examples</span>
+                                    </h1>
+                                    <p class="lead">The design is very human. Very easy to use.</p>
+                                </div>
+
                             </div>
                         </div>
-                        <div class="col-md-6">
-                            <div class="pl-md-5">
-                                <div class="icon icon-lg icon-shape icon-shape-warning shadow rounded-circle mb-5">
-                                    <i class="ni ni-settings-gear-65"></i>
-                                </div>
-                                <h3>Our customers</h3>
-                                <p class="lead">Don't let your uses guess by attaching tooltips and popoves to any
-                                    element.
-                                    Just make sure you enable them first via JavaScript.</p>
-                                <p> The kit comes with three pre-built pages to help you get started faster. You can
-                                    change the text and images and you're good to go. Just make sure you enable them
-                                    first via JavaScript.</p>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section-shaped my-2">
+                <div class="shape shape-style-2 shape-default shape-skew">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <div class="container shape-container d-flex">
+                    <div class="col px-0">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <h1 class="display-3  text-white">A beautiful Design System
+                                    <span>completed with examples</span>
+                                </h1>
+                                <p class="lead  text-white">The design system comes with four pre-built pages to help
+                                    you get started faster. You can change the text and images and you're good to
+                                    go.</p>
+                            </div>
+                            <div class="col-lg-6">
+                                <img src="../assets/section3.jpg" alt="Image" class="img-fluid">
                             </div>
                         </div>
                     </div>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -23,13 +23,13 @@
                             <div class="btn-wrapper text-center">
                                 <base-button size="lg" type="secondary" @click="goToSeniorForm">
                                     <img slot="icon" src="img/icons/common/senior.png"
-                                        style="width: 50px; height: auto;"> SENIOR </base-button>
+                                        style="width: 100px; height: auto;"> SENIOR </base-button>
                             </div>
                             <br>
                             <div class="btn-wrapper text-center">
                                 <base-button size="lg" type="secondary" @click="goToSeniorForm">
                                     <img slot="icon" src="img/icons/common/carePro.png"
-                                        style="width: 50px; height: auto;">CARE PROVIDER</base-button>
+                                        style="width: 100px; height: auto;">CARE PROVIDER</base-button>
                             </div>
                         </template>
 
