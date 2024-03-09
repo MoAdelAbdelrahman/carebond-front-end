404f579e539d5978fadf0f3e8477b58f802857ca
steven heung <stevenheung.ca@gmail.com>
2024-03-08 20:30:23 -0700

    Added Card Profile

--- src/views/SeniorHome.vue
+++ src/views/SeniorHome.vue
@@ -13,53 +13,7 @@
 
         <div class="container">
             <div class="row justify-content-center">
-                <div class="col-md-6">
-                    <a href="#/profile">
-                        <div class="col card card-lift--hover shadow border-0 mb-4" style="margin: 2px;">
-                            <section class="section section-skew">
-                                <div class="container">
-                                    <card shadow class="card-profile" no-body>
-                                        <div class="px-4">
-                                            <div class="card-profile-image">
-                                                <img v-lazy="'img/theme/pic.jpg'" alt="Profile Image"
-                                                    class="img-fluid rounded-circle">
-                                            </div>
-                                            <div class="card-body">
-                                                <h5 class="card-title">John Doe</h5>
-                                                <p class="card-text">Client Count: 100</p>
-                                                <p class="card-text">Rating: 4.5</p>
-                                            </div>
-                                        </div>
-                                    </card>
-                                </div>
-                            </section>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-6">
-                    <a href="#/profile">
-                        <div class="col card card-lift--hover shadow border-0 mb-4" style="margin: 2px;">
-                            <section class="section section-skew">
-                                <div class="container">
-                                    <card shadow class="card-profile" no-body>
-                                        <div class="px-4">
-                                            <div class="card-profile-image">
-                                                <img v-lazy="'img/theme/pic.jpg'" alt="Profile Image"
-                                                    class="img-fluid rounded-circle">
-                                            </div>
-                                            <div class="card-body">
-                                                <h5 class="card-title">John Doe</h5>
-                                                <p class="card-text">Client Count: 100</p>
-                                                <p class="card-text">Rating: 4.5</p>
-                                            </div>
-                                        </div>
-                                    </card>
-                                </div>
-                            </section>
-                        </div>
-                    </a>
-                </div>
-                
+                <cardprofile/>
             </div>
         </div>
     </section>
@@ -67,17 +21,18 @@
 
 <script>
 
-import profile from '@/views/Profile.vue'
+import cardprofile from './components/CardProfile.vue'
 
 export default {
     name: 'SeniorHome',
     data() {
+        'img/theme/pic.jpg'
         return {
 
         }
     },
     components: {
-        profile,
+        cardprofile,
     }
 }
 </script>
---
+++ src/views/components/CardProfile.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="col-md-6">
+        <div class="card card-lift--hover shadow border-0 mb-4  bg-gradient-gray" style="margin: 2px; max-width: 400px">
+            <section class="section section-skew">
+                <div class="container">
+                    <card shadow class="card-profile" no-body>
+                        <div class="px-4">
+                            <div class="card-profile-image text-center">
+                                <img v-lazy="profileImagePath" alt="Profile Image" class="img-fluid rounded-circle"
+                                    style="width: 100px; height: 100px; margin-top: 10px;">
+                            </div>
+                            <div class="card-body">
+                                <div class="text-center">
+                                    <h3 class="card-title" style="font-size: 24px; font-weight: 600;">{{ profileName }}</h3>
+                                </div>
+                                <div class="row">
+                                    <div class="col text-center">
+                                        <p class="card-text" style="color: #999999; font-weight: lighter;">Clients</p>
+                                        <h4 class="card-text" style="font-weight: 500;">{{ clientCount }}</h4>
+                                    </div>
+                                    <div class="col text-center">
+                                        <p class="card-text" style="color: #999999; font-weight: lighter;">Rating</p>
+                                        <h4 class="card-text" style="font-weight: 500; color: green;">{{ rating }}</h4>
+                                    </div>
+                                </div>
+                                <div class="text-center mt-4">
+                                    <a href="#/profile">
+                                        <base-button class="btn-primary">View Profile</base-button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </card>
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    profileName: String,
+    profileImagePath: String,
+    clientCount: Number,
+    rating: Number
+})
+</script>
